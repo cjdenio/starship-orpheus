50ad0565fef9f1e50b7af80b308866c06fddfcaf
Caleb Denio <cjdenio44@gmail.com>
2020-11-20 10:51:29 -0500

    ➕ Add TypeScript and Bolt

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { App } from "@slack/bolt";
+
+const app = new App({
+  token: process.env.SLACK_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  console.log("App started!");
+})();
