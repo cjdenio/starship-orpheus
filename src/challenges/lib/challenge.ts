85b12beb424e08c6f5acdf5e24a879d3b4e7a1bf
Caleb Denio <cjdenio44@gmail.com>
2021-01-27 20:59:35 -0500

    🗃 Switch to Postgres and TypeORM

--- src/challenges/lib/challenge.ts
+++ src/challenges/lib/challenge.ts
@@ -1,17 +1,14 @@
 import { App } from "@slack/bolt";
-import { MongoClient } from "mongodb";
 
 import Team from "../../types/team";
 
 abstract class Challenge {
   app: App;
-  db: MongoClient;
   index: number;
   team: Team;
 
-  constructor(app: App, db: MongoClient, index: number, team: Team) {
+  constructor(app: App, index: number, team: Team) {
     this.app = app;
-    this.db = db;
     this.index = index;
     this.team = team;
   }
